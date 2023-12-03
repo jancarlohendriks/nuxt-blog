823d5375e8ad6108a77b8a584a9379c4980b0b7f
jancarlohendriks <jancarlohendriks@hotmail.com>
2023-12-03 23:56:17 +0100

    storybook Button

--- components/Button.stories.js
+++ components/Button.stories.js
@@ -1,25 +1,73 @@
-import Button from "@/components/Button.vue";
-import { within, userEvent } from "@storybook/testing-library";
+import Button from "./Button.vue";
+import { action } from "@storybook/addon-actions";
+import { withKnobs, text, boolean, select } from "@storybook/addon-knobs";
+// import { withBackgrounds } from "@storybook/addon-backgrounds";
+// import { withDesign } from "storybook-addon-designs";
 
 export default {
-  title: "Button",
+  title: "Components/Button",
+  tags: ["autodocs"],
   component: Button,
-};
-
-export const Default = () => ({
-  components: { Button },
-  template: "<Button foo='asd'></Button>",
-});
-
-export const Primary = {
-  args: {
-    foo: "Primary",
+  decorators: [withKnobs],
+  // decorators: [withKnobs, withBackgrounds, withDesign],
+  parameters: {
+    // backgrounds: [
+    //   { name: "Default", value: "#ffffff", default: true },
+    //   { name: "Dark", value: "#333333" },
+    // ],
+    // design: {
+    //   type: "figma",
+    //   url: "your-figma-url",
+    // },
+    actions: {
+      handles: ["click .button"],
+    },
   },
 };
 
-export const Secondary = {
-  args: Primary.args,
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
+const Template = (args, { argTypes }) => ({
+  components: { Button },
+  props: {
+    label: {
+      default: text("Label", "Click me"),
+    },
+    disabled: {
+      default: boolean("Disabled", false),
+    },
+    type: {
+      default: select("Type", ["primary", "secondary"], "primary"),
+    },
+  },
+  template:
+    '<Button :label="label" :disabled="disabled" :type="type" @click="onClick" />',
+  methods: {
+    onClick: action("button-clicked"),
+  },
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+  type: "primary",
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  type: "secondary",
+};
+
+// Docs
+Primary.parameters = {
+  docs: {
+    description: {
+      story: "The primary button component.",
+    },
+  },
+};
+
+Secondary.parameters = {
+  docs: {
+    description: {
+      story: "The secondary button component.",
+    },
   },
 };
--- components/Button.vue
+++ components/Button.vue
@@ -1,7 +1,34 @@
+<!-- src/components/Button.vue -->
 <template>
-  <button>{{ foo }}</button>
+  <button class="button" @click="onClick">
+    {{ label }}
+  </button>
 </template>
 
-<script setup>
-defineProps(["foo"]);
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: "Click me",
+    },
+  },
+  methods: {
+    onClick() {
+      this.$emit("click");
+    },
+  },
+};
 </script>
+
+<style scoped>
+.button {
+  padding: 10px 15px;
+  font-size: 16px;
+  cursor: pointer;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+}
+</style>
